1710d799bc7f50f32770bec97a30a19f2e04e2e7
James Conlon <james.conlon@fr.fcm.travel>
2023-10-12 22:07:41 +0200

    change request to https

--- src/app/components/helpers.ts
+++ src/app/components/helpers.ts
@@ -2,7 +2,7 @@ import { GeocodingData } from '@/types';
 
 export const getGeocodedData = async (): Promise<Array<GeocodingData>> => {
 	const key = process.env.NEXT_PUBLIC_OPENWEATHERMAP_API_KEY;
-	const url = `http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appId=${key}`;
+	const url = `https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appId=${key}`;
 	const res = await fetch(url);
 	const data = await res.json();
 
