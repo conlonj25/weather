c7cf98fa5126d036eef8e922bbb3ce298d855dd3
James Conlon <james.conlon@fr.fcm.travel>
2023-11-03 13:12:19 +0100

    format nice weather window

--- src/app/components/Weather.tsx
+++ src/app/components/Weather.tsx
@@ -6,9 +6,10 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Search } from 'lucide-react';
 import { Card, CardContent } from '@/components/ui/card';
+import Image from 'next/image';
 
 export const Weather = () => {
-	const [query, setQuery] = useState('london');
+	const [query, setQuery] = useState('');
 	const [city, setCity] = useState<CityData>();
 	const [weather, setWeather] = useState<WeatherData>();
 
@@ -19,7 +20,7 @@ export const Weather = () => {
 		setCity(c);
 
 		if (city) {
-			const w = await getWeatherData(city.lat, city.lon);
+			const w = await getWeatherData(city.lat, city.lon, 'metric');
 			setWeather(w);
 		}
 	};
@@ -47,28 +48,56 @@ export const Weather = () => {
 		return json.data;
 	};
 
+	const weatherIconUrl = weather
+		? `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
+		: '';
+
 	return (
 		<div className="flex flex-col gap-4">
-			<Card>
+			<Card className="bg-gradient-to-r from-orange-400 via-oramge-500 to-orange-400 shadow-md">
 				<CardContent className="flex justify-center items-center p-6">
-					<div className="flex w-full max-w-sm items-center space-x-2">
+					<div className="flex w-full max-w-sm items-center space-x-3">
 						<Input
 							type="text"
 							placeholder="search city"
 							value={query}
 							onChange={(e) => setQuery(e.target.value)}
 						/>
-						<Button onClick={handleSubmit}>
+						<Button
+							className="bg-orange-400 hover:bg-orange-500 border-2"
+							onClick={handleSubmit}
+						>
 							<Search />
 						</Button>
 					</div>
 				</CardContent>
 			</Card>
-			<Card>
-				<CardContent>
-					<p>{JSON.stringify(weather)}</p>
-				</CardContent>
-			</Card>
+			{weather && (
+				<Card className="bg-secondary">
+					<div className="flex justify-around m-2">
+						<div className="flex flex-col justify-center">
+							<Image
+								src={weatherIconUrl}
+								alt={'weather icon'}
+								width={100}
+								height={100}
+							/>
+							<h1 className="text-center">{weather.weather[0].main}</h1>
+						</div>
+						<div className="flex flex-col justify-center">
+							<h1 className="text-center text-5xl">
+								{`${Math.round(weather.main.temp)}°C`}
+							</h1>
+						</div>
+						<div className="flex flex-col justify-center gap-1 text-xs text-muted-foreground">
+							<p>{`Min: ${Math.round(weather.main.temp_min)}°C`}</p>
+							<p>{`Max: ${Math.round(weather.main.temp_max)}°C`}</p>
+							<p>{`Humidity: ${weather.main.humidity}%`}</p>
+							<p>{`Wind: ${weather.wind.speed} km/h`}</p>
+						</div>
+					</div>
+				</Card>
+			)}
 		</div>
 	);
 };
